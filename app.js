0bbf5ee61b787281159063ad7c63eab876fce1bc
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:51:20 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Lista de reproducción')
+
+alert('Creación de playlist de música')
+
+const nombrePlaylist = prompt('Ingresa el nombre de la playlist');
+const cancionUno = prompt('Ingresa una canción');
+const cancionDos = prompt('Ingresa una canción');
+const cancionTres = prompt('Ingresa una canción');
+
+
+alert(`Se ha creado la playlist '${nombrePlaylist}' con las canciones: 
+    ${cancionUno}, 
+    ${cancionDos},
+    ${cancionTres}.
+    `)
